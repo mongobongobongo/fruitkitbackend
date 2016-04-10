3ac690796b684875f9b4a277ce0f3d5250958f0f
kalle <lordflak@gmail.com>
2016-04-10 14:48:59 +0300

    cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
