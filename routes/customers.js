9467cd30582d451e4f79e9d717dfd12aed5b7430
kalle <lordflak@gmail.com>
2016-04-10 13:41:53 +0300

    mongoConnect added

--- routes/customers.js
+++ routes/customers.js
@@ -1,22 +1,28 @@
 var express = require('express');
+var MongoConnect = require('../lib/mongoConnect.js');
 var router = express.Router();
-var promise = require('promise');
+var collection = 'customers';
 
 /* RETRIEVE customer listing. */
 router.get('/', function (req, res, next) {
-        var baa = mongoFetch('customers', undefined, function (err, customers) {
+        var baa = MongoConnect.retrieve(collection, undefined, function (err, customers) {
             if (err) {
-
+                throw err;
             } else {
-
                 res.status(200).send(customers)
             }
         });
     })
     .post ('/', function (req, res, next) {
         var body = req.body;
-        mongoCreate('customers', body);
-        res.send('Created Customer. Now go and fetch it!');
+        MongoConnect.create(collection, body, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(result);
+            }
+        });
+
     });
 
 
@@ -24,9 +30,9 @@ router.get('/', function (req, res, next) {
 router.get('/:id', function (req, res, next) {
 
         var params = req.params;
-        var baa = mongoFetch('customers', {"_id": ObjectId(params.id)}, function (err, customers) {
+        var baa = MongoConnect.retrieve(collection, {"_id": ObjectId(params.id)}, function (err, customers) {
             if (err) {
-
+                throw err;
             } else {
                 res.status(200).send(customers);
             }
@@ -34,96 +40,40 @@ router.get('/:id', function (req, res, next) {
     })
     .put ('/:id', function (req, res, next) {
         var params = req.params;
+        //TODO: Update
+        //Create a payload object for update function.
+        var id = {_id: ObjectId(params.id)};
+        if (typeof body !== Object) {
+            body = JSON.parse(body);
+        }
+        MongoConnect.update(collection, id, body, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(result);
+            }
+        })
         res.status(200).send('Updated Single Customer: ', params.id);
     })
     .delete ('/:id', function (req, res, next) {
         var params = req.params;
-        res.status(200).send('Deleted user with id: ', params.id);
+        //TODO: Delete
+
+        var payload = {};
+        var id = {_id: ObjectId(params.id)};
+        if (typeof body !== Object) {
+            body = JSON.parse(body);
+        }
+        extend(true, payload, id, body);
+
+        MongoConnect.delete(collection, payload, function (err, customer) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send();
+            }
+        });
     });
 
 module.exports = router;
-
-
-/* FIND A BETTER PLACE FOR THIS!!! */
-
-
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/fruitkit';
-
-var ObjectId = require('mongodb').ObjectID;
-
-
-function retrieve(db, collection, query, callback) {
-    var cursor;
-    var data = [];
-    if (query == undefined) {
-        cursor = db.collection(collection).find();
-    } else {
-        cursor = db.collection(collection).find(query);
-    }
-
-    cursor.each(function (err, doc) {
-        assert.equal(err, null);
-        if (doc != null) {
-            data.push(doc);
-        } else {
-            callback(data);
-        }
-    });
-
-};
-
-
-function mongoFetch(collection, query, callbackfunction) {
-
-
-    var results = null;
-
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        results = retrieve(db, collection, query, function (results) {
-            callbackfunction(err, results);
-            db.close();
-
-        });
-    });
-};
-
-function mongoUpdate() {
-
-};
-
-function mongoCreate(collection, payload) {
-    //Transform the text to a usable json object
-    //payload = JSON.parse(payload);
-    console.log ("Creating new thing");
-    console.log (payload);
-    
-
-    function insertDocument(db, collection, payload, callback) {
-        console.log (collection);
-        db.collection(collection).insertOne(payload, function (err, result) {
-            assert.equal(err, null);
-            console.log("Inserted a document into the restaurants collection. Go fetch it yo!");
-            callback();
-        });
-    };
-
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        console.log ("here so far");
-        insertDocument(db, collection, payload, function () {
-            db.close();
-        });
-    });
-
-
-};
-
-function mongoDelete() {
-
-};
-
 
--- routes/packages.js
+++ routes/packages.js
@@ -1,24 +1,79 @@
 var express = require('express');
+var MongoConnect = require('../lib/mongoConnect.js');
 var router = express.Router();
+var collection = 'packages';
 
-/* RETRIEVE users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Retrieved All Packages');
-})
-    .post ('/', function(req, res, next) {
-  res.send('Created Package');
-});
+/* RETRIEVE customer listing. */
+router.get('/', function (req, res, next) {
+        var baa = MongoConnect.retrieve(collection, undefined, function (err, customers) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(customers)
+            }
+        });
+    })
+    .post ('/', function (req, res, next) {
+        var body = req.body;
+        MongoConnect.create(collection, body, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(result);
+            }
+        });
 
-/* RETRIEVE Single Package */
-router.get('/:id', function(req, res, next) {
-  res.send('Retrieved Package');
-})
-    .put ('/:id', function(req, res, next) {
-  res.send('Updated Package');
-})
-    .delete ('/:id', function(req, res, next) {
-  res.send('Deleted Package');
-});
+    });
 
+
+/* RETRIEVE customer specific user. */
+router.get('/:id', function (req, res, next) {
+
+        var params = req.params;
+        var baa = MongoConnect.retrieve(collection, {"_id": ObjectId(params.id)}, function (err, customers) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(customers);
+            }
+        });
+    })
+    .put ('/:id', function (req, res, next) {
+        var params = req.params;
+        //TODO: Update
+        //Create a payload object for update function.
+        var id = {_id: ObjectId(params.id)};
+        if (typeof body !== Object) {
+            body = JSON.parse(body);
+        }
+        MongoConnect.update(collection, id, body, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send(result);
+            }
+        })
+        res.status(200).send('Updated Single Customer: ', params.id);
+    })
+    .delete ('/:id', function (req, res, next) {
+        var params = req.params;
+        //TODO: Delete
+
+        var payload = {};
+        var id = {_id: ObjectId(params.id)};
+        if (typeof body !== Object) {
+            body = JSON.parse(body);
+        }
+        extend(true, payload, id, body);
+
+        MongoConnect.delete(collection, payload, function (err, customer) {
+            if (err) {
+                throw err;
+            } else {
+                res.status(200).send();
+            }
+        });
+    });
 
 module.exports = router;
+
