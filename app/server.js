dab6378df06952daf70f8e5621186ac0b5daa56d
marharyta <marharyta.obraztsova@gmail.com>
2016-10-02 15:23:16 +0300

    hopefully, fixed server.js

--- app/server.js
+++ app/server.js
@@ -165,8 +165,11 @@ function onListening() {
   console.log ('Listening on ' + bind);
 }
 
+// demo Route (GET http://localhost:8080)
+// ...
+ 
 // connect to database
-//mongoose.connect(config.database);
+mongoose.connect(config.database);
  
 // pass passport for configuration
 require('./config/passport')(passport);
@@ -195,36 +198,3 @@ apiRoutes.post('/signup', function(req, res) {
  
 // connect the api routes under /api/*
 app.use('/api', apiRoutes);
-// connect to database
-//mongoose.connect(config.database);
- 
-// pass passport for configuration
-require('./config/passport')(passport);
- 
-// bundle our routes
-var apiRoutes = express.Router();
- 
-// create a new user account (POST http://localhost:8080/api/signup)
-apiRoutes.post('/signup', function(req, res) {
-  if (!req.body.name || !req.body.password) {
-    res.json({success: false, msg: 'Please pass name and password.'});
-  } else {
-    var newUser = new User({
-      name: req.body.name,
-      password: req.body.password
-    });
-    // save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
-      }
-      res.json({success: true, msg: 'Successful created new user.'});
-    });
-  }
-});
- 
-// connect the api routes under /api/*
-app.use('/api', apiRoutes);
-
-
-module.exports = app;
