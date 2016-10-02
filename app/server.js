bc765b822cbbc47e791f5b80de33e4c0e1f4551d
marharyta <marharyta.obraztsova@gmail.com>
2016-10-02 14:29:47 +0300

    added dummy server port

--- app/server.js
+++ app/server.js
@@ -10,6 +10,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var jwt = require('jwt-simple');
 var debug = require('debug')('fruitkit:server');
+
 /**
  * Get port from environment and store in Express.
  */
@@ -163,6 +164,67 @@ function onListening() {
   debug('Listening on ' + bind);
   console.log ('Listening on ' + bind);
 }
+
+// connect to database
+mongoose.connect(config.database);
+ 
+// pass passport for configuration
+require('./config/passport')(passport);
+ 
+// bundle our routes
+var apiRoutes = express.Router();
+ 
+// create a new user account (POST http://localhost:8080/api/signup)
+apiRoutes.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+ 
+// connect the api routes under /api/*
+app.use('/api', apiRoutes);
+// connect to database
+mongoose.connect(config.database);
+ 
+// pass passport for configuration
+require('./config/passport')(passport);
+ 
+// bundle our routes
+var apiRoutes = express.Router();
+ 
+// create a new user account (POST http://localhost:8080/api/signup)
+apiRoutes.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+ 
+// connect the api routes under /api/*
+app.use('/api', apiRoutes);
 
 // connect to database
 mongoose.connect(config.database);
